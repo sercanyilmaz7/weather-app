8d54ec5432174ee7d229e564d76e7eb246403a9f
sercanyilmaz7 <srcn.yilmaz333@gmail.com>
2023-01-31 13:46:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+const input = document.querySelector("#input");
+const button = document.querySelector(".button");
+const delete1 = document.querySelector(".delete");
+const container = document.querySelector(".container");
+const form = document.querySelector(".form");
+
+let cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+window.addEventListener("load", () => {
+  getCityListFromLocalStorage();
+});
+
+const getCityListFromLocalStorage = () => {
+  cities.forEach((city) => {
+    createWeather(city);
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  // console.log(input.value);
+  e.preventDefault();
+  cityinput = input.value;
+  //   console.log(cityinput);
+  if (cityinput.trim() === "") {
+    alert("please enter a city");
+  } else {
+    getFetchData();
+  }
+
+  //   cityinput = input.value;
+  form.reset();
+  input.focus();
+});
+
+const getFetchData = () => {
+  let API = "8f476966e75f896c4c73cf65cc1e398b";
+  let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityinput}&appid=${API}&units=metric`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => createWeatherInfo(data));
+};
+
+const createWeatherInfo = (data) => {
+  const {
+    name,
+    sys: { country },
+    main: { temp },
+    weather: [{ icon, description }],
+  } = data;
+
+  const cityData = {
+    name: name,
+    country: country,
+    temp: temp,
+    icon: icon,
+    description: description,
+  };
+
+  //   cityList.forEach((e) => {
+  //     if (e.name == cityInfo.name) {
+  //       alert("Enter anoter city");
+  //     } else {
+  //       createWeather(cityInfo);
+  //       cityList.push(cityInfo);
+
+  //       localStorage.setItem("cityList", JSON.stringify(cityList));
+  //     }
+  //   });
+  let flag = true;
+  cities.forEach((x) => x.name == cityData.name && (flag = false));
+  if (flag) {
+    createWeather(cityData);
+    cities.push(cityData);
+    localStorage.setItem("cities", JSON.stringify(cities));
+  } else {
+    alert("Please enter another city");
+  }
+};
+
+const createWeather = (cityData) => {
+  //   console.log(data);
+  const { name, country, temp, icon, description } = cityData;
+  //   console.log(description);
+  container.innerHTML += `
+    <div class="card">
+    <span>${name}</span>
+    <span>${country}</span>
+    <span style="font-size: 1.5rem">${temp} &#8451 </span>
+    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+    <span>${description}</span>
+    <button class="btn">X</button>
+    </div>`;
+};
+
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    console.log(cities);
+    console.log(e.target.parentElement.children[0].innerText);
+    e.target.closest("div").remove();
+    cities = cities.filter((x) => {
+      return x.name != e.target.parentElement.children[0].innerText;
+    });
+    localStorage.setItem("cities", JSON.stringify(cities));
+    console.log(cities);
+  }
+});
+
+delete1.addEventListener("click", (e) => {
+  container.innerHTML=""
+  cities = [];
+  localStorage.setItem("cities", JSON.stringify(cities));
+});
